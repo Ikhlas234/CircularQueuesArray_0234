4d97b2be322b4be9591093ebd0cdb4a44daf10c2
Ikhlas234 <ikhsanwed@gmail.com>
2025-06-10 10:42:35 +0700

    memberikan kelas queues

--- CirculaQueuest.cpp
+++ CirculaQueuest.cpp
@@ -5,3 +5,24 @@ using namespace std;
 class Queues {
     int FRONT, REAR, max = 5;
     int queue_arry[5];
+
+public:
+Queues() {
+    FRONT = -1;
+    REAR = -1;
+
+}
+ void insert() {
+    int num;
+    cour << "Enter a number: ";
+    cin >> num;
+    cout << endl;
+
+    // cek apakah antrina penuh
+    if ((FRONT == 0 && REAR == max - 1) || FRONT == REAR + 1) {
+        cout << "\nQueue overflow\n";
+        
+    }
+    
+ }
+}
